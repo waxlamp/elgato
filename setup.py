e5adb779f88208df1ca03592989abae7af702594
Roni Choudhury <roni.choudhury@kitware.com>
2020-09-05 18:14:29 -0400

    Run formatter

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name = "elgato",
-    version = "0.1.0",
-    author = "Roni Choudhury",
-    author_email = "aichoudh@gmail.com",
-    packages = ["elgato"],
-    entry_points = {
-        "console_scripts": [
-            "elgato=elgato.__main__:main"
-        ],
+    name="elgato",
+    version="0.1.0",
+    author="Roni Choudhury",
+    author_email="aichoudh@gmail.com",
+    packages=["elgato"],
+    entry_points={
+        "console_scripts": ["elgato=elgato.__main__:main"],
     },
-    url = "https://github.com/waxlamp/elgato",
-    license = "LICENSE",
-    description = "Control script for El Gato brand keylights",
-    long_description = open("README.md").read(),
-    install_requires = [
+    url="https://github.com/waxlamp/elgato",
+    license="LICENSE",
+    description="Control script for El Gato brand keylights",
+    long_description=open("README.md").read(),
+    install_requires=[
         "leglight == 0.2.0",
     ],
 )
