ef7fd547432daf1034c913c5aeeeee490a4d5bc3
Roni Choudhury <roni.choudhury@kitware.com>
2021-04-11 22:41:20 -0400

    Add a package output

--- flake.nix
+++ flake.nix
@@ -6,12 +6,50 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    leglight = pkgs.python38Packages.buildPythonPackage rec {
+      pname = "leglight";
+      version = "0.2.0";
+
+      src = pkgs.python38Packages.fetchPypi {
+        inherit pname version;
+        sha256 = "41ab462fe12e2ec3e02ff29a00316f822b078c331c70bd36a635568bd1c4204a";
+      };
+
+      propagatedBuildInputs = with pkgs.python38Packages; [
+        zeroconf
+        requests
+      ];
+
+      doCheck = false;
+    };
   in {
     devShell.x86_64-linux = pkgs.mkShell {
       buildInputs = with pkgs; [
         python38
         pipenv
       ];
+    };
+
+    defaultPackage.x86_64-linux = pkgs.python38Packages.buildPythonPackage rec {
+      pname = "PyElgato";
+      version = "1.2.0";
+
+      src = pkgs.python38Packages.fetchPypi {
+        inherit pname version;
+        sha256 = "7834d4c6dac7a1646b5238dcec187645d6cd4d8dc4e2f3f9e0d789a3bddd369c";
+      };
+
+      buildInputs = with pkgs.python38Packages; [
+        setuptools-scm
+        wheel
+      ];
+
+      propagatedBuildInputs = [
+        leglight
+      ];
+
+      doCheck = false;
     };
   };
 }
